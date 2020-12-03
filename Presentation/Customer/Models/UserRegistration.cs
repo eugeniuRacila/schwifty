bad8cec2e58100efe275219850ea15ad3dcc1155
Eugeniu Racila <eugeniu.racila@gmail.com>
2020-12-03 11:24:12 +0100

    [SEP-6] Create front-end for customer registration * Created front-end interface for customer registration * Created user registration model

---
+++ Presentation/Customer/Models/UserRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Models
+{
+    public class UserRegistration
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
