e958dbf6699d51608ffbcd8d979d16c983a3213b
Igork777 <igorlopatchin27@gmail.com>
2020-12-15 18:18:15 +0100

    SQL to HQL

--- Spring-Web-Service/src/main/java/project/sep3/DAO/customers/CustomerDAO.java
+++ Spring-Web-Service/src/main/java/project/sep3/DAO/customers/CustomerDAO.java
@@ -1,6 +1,6 @@
 package project.sep3.DAO.customers;
 
-import project.sep3.entities.Customer;
+import project.sep3.models.Customer;
 
 public interface CustomerDAO {
     Customer create(String firstName, String lastName, String email, String password);
